f2bd1ae0b341d814003647571fb21eac357d7fca
AnsonH <ansonheung2000@gmail.com>
2022-10-02 20:56:42 +0800

    fix(redis): env not loaded

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,4 +1,6 @@
+import * as dotenv from "dotenv";
 import Redis from "ioredis";
+dotenv.config();
 
 const redis = new Redis(parseInt(process.env.REDIS_PORT, 10) || 6379, process.env.REDIS_HOST ?? '127.0.0.1');
 
